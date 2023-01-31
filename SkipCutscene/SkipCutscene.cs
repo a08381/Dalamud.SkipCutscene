f39b78e7d898144047cdd21aad456c3df3797a49
a08381 <632785425@163.com>
2023-02-01 03:37:55 +0800

    bump API 8.

--- SkipCutscene/SkipCutscene.cs
+++ SkipCutscene/SkipCutscene.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
 using System.Security.Cryptography;
 using Dalamud;
 using Dalamud.Game;
@@ -15,10 +17,12 @@ namespace Plugins.a08381.SkipCutscene
     {
 
         private readonly Config _config;
-        private readonly RNGCryptoServiceProvider _csp;
+        private readonly RandomNumberGenerator _csp;
 
         private readonly decimal _base = uint.MaxValue;
 
+        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+        [SuppressMessage("ReSharper", "AssignNullToNotNullAttribute")]
         public SkipCutscene()
         {
             if (Interface.GetPluginConfig() is not Config configuration || configuration.Version == 0)
@@ -30,7 +34,7 @@ namespace Plugins.a08381.SkipCutscene
 
             Address.Setup(SigScanner);
 
-            if (Address.Offset1 != IntPtr.Zero && Address.Offset2 != IntPtr.Zero)
+            if (Address.Valid)
             {
                 PluginLog.Information("Cutscene Offset Found.");
                 if (_config.IsEnabled)
@@ -43,8 +47,7 @@ namespace Plugins.a08381.SkipCutscene
                 Dispose();
                 return;
             }
-
-            _csp = new RNGCryptoServiceProvider();
+            _csp = RandomNumberGenerator.Create();
 
             CommandManager.AddHandler("/sc", new CommandInfo(OnCommand)
             {
@@ -59,24 +62,24 @@ namespace Plugins.a08381.SkipCutscene
         }
 
         public string Name => "SkipCutscene";
-
+        
         [PluginService]
         public DalamudPluginInterface Interface { get; private set; }
-
+        
         [PluginService]
         public SigScanner SigScanner { get; private set; }
 
         [PluginService]
         public CommandManager CommandManager { get; private set; }
-
+        
         [PluginService]
         public ChatGui ChatGui { get; private set; }
 
-        public CutsceneAddressResolver Address { get; private set; }
+        public CutsceneAddressResolver Address { get; }
 
         public void SetEnabled(bool isEnable)
         {
-            if (Address.Offset1 == IntPtr.Zero || Address.Offset2 == IntPtr.Zero) return;
+            if (!Address.Valid) return;
             if (isEnable)
             {
                 SafeMemory.Write<short>(Address.Offset1, -28528);
@@ -107,6 +110,8 @@ namespace Plugins.a08381.SkipCutscene
     public class CutsceneAddressResolver : BaseAddressResolver
     {
 
+        public bool Valid => Offset1 != IntPtr.Zero && Offset2 != IntPtr.Zero;
+
         public IntPtr Offset1 { get; private set; }
         public IntPtr Offset2 { get; private set; }
 
@@ -116,11 +121,11 @@ namespace Plugins.a08381.SkipCutscene
             Offset2 = sig.ScanText("74 18 8B D7 48 8D 0D");
             PluginLog.Information(
                 "Offset1: [\"ffxiv_dx11.exe\"+{0}]",
-                (Offset1.ToInt64() - Process.GetCurrentProcess().MainModule.BaseAddress.ToInt64()).ToString("X")
+                (Offset1.ToInt64() - Process.GetCurrentProcess().MainModule!.BaseAddress.ToInt64()).ToString("X")
                 );
             PluginLog.Information(
                 "Offset2: [\"ffxiv_dx11.exe\"+{0}]",
-                (Offset2.ToInt64() - Process.GetCurrentProcess().MainModule.BaseAddress.ToInt64()).ToString("X")
+                (Offset2.ToInt64() - Process.GetCurrentProcess().MainModule!.BaseAddress.ToInt64()).ToString("X")
                 );
         }
 
